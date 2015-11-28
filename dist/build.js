42136c48e8cf182238101c1dacc8981a18d01bba
Justin Barabander <jbarabander@gmail.com>
2015-11-28 16:59:21 -0500

    created modularized version

---
+++ dist/build.js
@@ -0,0 +1,83 @@
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
+    module.exports = 'ng-counter-directive';
+}
+
+(function (window, angular) {
+    'use strict';
+    var app = angular.module('ng-counter-directive', ['ui.bootstrap']);
+
+    app.directive('counter', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/src/directives/counter/counter.html',
+            scope: {
+                max: '=',
+                min: '=',
+                step: '='
+            },
+            link: function (scope, element, attrs) {
+                scope.dropdown = element.children()[0].lastElementChild;
+                scope.currentIndex = null;
+                scope.quantity = null;
+                scope.showInput = false;
+                scope.options = _.range(scope.min, scope.max, scope.step);
+
+                //helper functions
+                function setIndexAndQuantity(quantity, index) {
+                    scope.quantity = quantity;
+                    scope.currentIndex = index;
+                }
+
+                function willGoAboveMax() {
+                    return scope.quantity + scope.step >= scope.max;
+                }
+
+                function willGoBelowMin() {
+                    return scope.quantity - scope.step <= scope.min;
+                }
+
+                scope.goUpOneStep = function () {
+                    if (scope.quantity === null) {
+                        setIndexAndQuantity(scope.min, 0);
+                    }
+                    else if (willGoAboveMax()) {
+                        setIndexAndQuantity(scope.max, scope.options.length - 1);
+                    }
+                    else {
+                        var index = scope.options.indexOf(scope.quantity) + 1;
+                        setIndexAndQuantity(scope.options[index], index);
+                    }
+                };
+                scope.goDownOneStep = function () {
+                    if (scope.quantity === null || willGoBelowMin()) {
+                        setIndexAndQuantity(scope.min, 0);
+                    }
+                    else {
+                        var index = scope.options.indexOf(scope.quantity) - 1;
+                        setIndexAndQuantity(scope.options[index], index);
+                    }
+                };
+                if (scope.options.indexOf(scope.max) === -1) {
+                    scope.options.push(scope.max);
+                }
+                scope.toggleInput = function () {
+                    scope.showInput = true;
+                };
+                scope.setInputTo = function (choice, index) {
+                    scope.quantity = choice;
+                    scope.currentIndex = index;
+                };
+                scope.goToCurrentEl = (function () {
+                    var previousIndex;
+                    return function () {
+                        if (scope.currentIndex !== null && scope.currentIndex !== previousIndex) {
+                            previousIndex = scope.currentHash;
+                            var currentEl = scope.dropdown.children[scope.currentIndex];
+                            scope.dropdown.scrollTop = currentEl.offsetTop;
+                        }
+                    }
+                })();
+            }
+        }
+    });
+})(window, window.angular);
