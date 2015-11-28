5a709179f4aafd96ca6030f8c18c05436b378419
Justin Barabander <jbarabander@gmail.com>
2015-11-28 15:47:28 -0500

    Added helper functions and more cleanup

--- src/directives/counter/counter.js
+++ src/directives/counter/counter.js
@@ -9,35 +9,42 @@ app.directive('counter', function ($location, $anchorScroll) {
         },
         link: function (scope, element, attrs) {
             scope.dropdown = element.children()[0].lastElementChild;
-            scope.currentHash = null;
+            scope.currentIndex = null;
             scope.quantity = null;
             scope.showInput = false;
             scope.options = _.range(scope.min, scope.max, scope.step);
-            
+
+            //helper functions
+            function setIndexAndQuantity(quantity, index) {
+                scope.quantity = quantity;
+                scope.currentIndex = index;
+            }
+            function willGoAboveMax() {
+                return scope.quantity + scope.step >= scope.max;
+            }
+            function willGoBelowMin() {
+                return scope.quantity - scope.step <= scope.min;
+            }
+
             scope.goUpOneStep = function () {
                 if (scope.quantity === null) {
-                    scope.quantity = scope.min;
-                    scope.currentHash = 0;
+                    setIndexAndQuantity(scope.min, 0);
                 }
-                else if (scope.quantity + scope.step >= scope.max) {
-                    scope.quantity = scope.max;
-                    scope.currentHash = scope.options.length - 1;
+                else if (willGoAboveMax()) {
+                    setIndexAndQuantity(scope.max, scope.options.length - 1);
                 }
                 else {
                     var index = scope.options.indexOf(scope.quantity) + 1;
-                    scope.quantity = scope.options[index];
-                    scope.currentHash = index;
+                    setIndexAndQuantity(scope.options[index], index);
                 }
             };
             scope.goDownOneStep = function () {
-                if (scope.quantity === null || scope.quantity - scope.step <= scope.min) {
-                    scope.quantity = scope.min;
-                    scope.currentHash = 0;
+                if (scope.quantity === null || willGoBelowMin()) {
+                    setIndexAndQuantity(scope.min, 0);
                 }
                 else {
                     var index = scope.options.indexOf(scope.quantity) - 1;
-                    scope.quantity = scope.options[index];
-                    scope.currentHash = index;
+                    setIndexAndQuantity(scope.options[index], index);
                 }
             };
             if (scope.options.indexOf(scope.max) === -1) {
@@ -48,14 +55,14 @@ app.directive('counter', function ($location, $anchorScroll) {
             };
             scope.setInputTo = function (choice, index) {
                 scope.quantity = choice;
-                scope.currentHash = index;
+                scope.currentIndex = index;
             };
             scope.goToCurrentEl = (function () {
                 var previousIndex;
                 return function () {
-                    if (scope.currentHash !== null && scope.currentHash !== previousIndex) {
+                    if (scope.currentIndex !== null && scope.currentIndex !== previousIndex) {
                         previousIndex = scope.currentHash;
-                        var currentEl = scope.dropdown.children[scope.currentHash];
+                        var currentEl = scope.dropdown.children[scope.currentIndex];
                         scope.dropdown.scrollTop = currentEl.offsetTop;
                     }
                 }
